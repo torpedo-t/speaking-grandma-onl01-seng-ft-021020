b483ce7a0f3745b9e9d0eee9131ab79c4ef92ec5
Thomas John Helin <thomashelin@yahoo.com>
2020-02-12 15:25:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,10 @@
+def speak_to_grandma(phrase)
+if phrase == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN"
+end
+if phrase == phrase.upcase
+  return "NO, NOT SINCE 1938!"
+else  
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
