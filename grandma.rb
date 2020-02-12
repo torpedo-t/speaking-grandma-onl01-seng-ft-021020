063cc7512ba2c15109b5e3ab014cc5e443552315
Thomas John Helin <thomashelin@yahoo.com>
2020-02-12 15:26:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN"
+  return "I LOVE YOU TOO PUMPKIN!"
 end
 if phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
